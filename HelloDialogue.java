a1442e6fca2f509970884064592ebb606929cac0
BrianGio <38891888+BrianGio@users.noreply.github.com>
2018-09-17 12:59:16 -0400

    Add files via upload

---
+++ HelloDialogue.java
@@ -0,0 +1,13 @@
+/* Filename: HelloDialog.java
+  Authour: Brian
+  Date: 7/17/18
+  Program that display GUI dialog message */
+  import javax.swing.JOptionPane;
+  
+  public class HelloDialog
+  {
+	 public static void main(String[] args)
+	 {
+		JOptionPane.showMessageDialog(null, "Hello, World!"); 
+	 }
+   }
---
+++ MovieQuoteInfo.java
@@ -0,0 +1,10 @@
+public class MovieQuoteInfo
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Stop looking at me swan!")
+		System.out.println("X         @@           X")
+		System.out.println("")
+		System.out.println("")
+	}
+}
